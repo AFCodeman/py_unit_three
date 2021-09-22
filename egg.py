fe34a18aee62bd21792df7b8238762ce74340d22
anthonyfagiolo <fagioan@ssfs.org>
2021-09-22 15:15:10 -0400

    trying to make an egg - not doing well

--- egg.py
+++ egg.py
@@ -1,5 +1,18 @@
 # Start writing your functions below this line
 
+def topegg():
+    print:("
+           __________
+          /          \
+         /            \"")
+
+def bottomegg():
+    print("\            /
+         \          /
+          __________")
+
+def line():
+    print("_''_'_''__'_")
 
 def main():
     # all of your function calls should go here. Remove the word "pass" before adding function calls.
@@ -7,3 +20,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+print(topegg())
