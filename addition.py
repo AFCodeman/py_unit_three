74b58719c1548a409aa6e00e8e6dd609b3fc8916
anthonyfagiolo <fagioan@ssfs.org>
2021-09-24 09:00:08 -0400

    did simple addition and then tested it

--- addition.py
+++ addition.py
@@ -1,9 +1,15 @@
 # Write your addition function in the space below...
-
+def add_two(x,y):
+    sum=x+y
+    print(sum)
 
 # Do not change anything below these lines
 def main():
     pass
 
+
 if __name__ == '__main__':
     main()
+
+add_two(2,4)
+add_two(12,9)
--- volume_cone.py
+++ volume_cone.py
@@ -1,5 +1,8 @@
 # Write your cone volume function in the space below...
-
+pi=3.14
+r=10
+h=3
+V=pi*r**2(h/3)
 
 
 # Do not change anything below these lines
