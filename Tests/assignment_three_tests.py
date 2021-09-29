f11e4a912f89211d9fbc1f872c68828294a115de
anthonyfagiolo <fagioan@ssfs.org>
2021-09-29 11:54:51 -0400

    almost finished assignment three

--- Tests/assignment_three_tests.py
+++ Tests/assignment_three_tests.py
@@ -1,15 +1,16 @@
 import unittest
-import assignment_three
+from assignment_three import rectangle_area
+from assignment_three import surface_area
 
 
 class MyTestCase(unittest.TestCase):
     def test_rectangle_area(self):
-        self.assertEqual(24,assignment_three.rectangle_area(4,6))  # add assertion here
-        self.assertEqual(49,assignment_three.rectangle_area(7,7))
+        self.assertEqual(24,rectangle_area(4,6))  # add assertion here
+        self.assertEqual(49,rectangle_area(7,7))
 
-    def test_cube_area(self):
-        self.assertEqual()
-        self.assertEqual()
+    def test_surface_area(self):
+        self.assertEqual(22,surface_area(1,2,3))
+        self.assertEqual(40,surface_area(4,2,2))
 
 
 if __name__ == '__main__':
--- assignment_three.py
+++ assignment_three.py
@@ -4,10 +4,49 @@ def rectangle_area(b,h):
     area=b*h
     return area
 
-def surface_area(b,h,w)
+def surface_area(b,h,w):
+    """
+    ses the base, width, and height of rectangle to calculate the surface area.
+    calls back the rectangle_area function
+    :param b: the base of the rectangle
+    :param h: the height of the rectangle
+    :param w: the width of the rectangle
+    :return: the surface area of the rectangle
+    """
     top=rectangle_area(w,h)*2
     left=rectangle_area(b,w)*2
     frnt=rectangle_area(b,h)*2
     area=top+left+frnt
     return area
+
+def surface_input():
+   """
+   User inputs the base, height, and width to find the surface arae.
+   calls back surface_area and rectangle_area function
+   :return: surface area of 3D Rectangle
+   """
+   b = float(input("What is the base of the rectangle?"))
+   h = float(input("What is the height of the rectangle?"))
+   w = float(input("What is the width of the rectangle?"))
+   print(surface_area(b,h,w))
+
+def instruction_manual():
+    print("This program is made to find the surface area of a 3 dimensional object. The functions called surface_area and rectangle_area both calculate surface area, and surface_input is the function that gets input.")
+    print("To calculate, you must multiply base times height, base times width, and width times height. They all must be parameters")
+    print("Then for input, make sure to set base, height, and width as variables.")
+
+def get_height():
+    h = float(input("What is the height of the rectangle?"))
+
+def get_base():
+    b = float(input("What is the base of the rectangle?"))
+
+def get_width():
+    w = float(input("what is the width of the rectangle?"))
+
+
+def main():
+    pass
+
+surface_input()
 
